f9241471a9fd100ef9e6f2075b66cb00587b31b1
Rishi Raj <rishiraj007bgp@gmail.com>
2024-01-26 12:54:47 +0530

    geet 20 images on request

--- memory_card/src/App.jsx
+++ memory_card/src/App.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react'
 import './index.css'
 
 function App() {
+  const [imgData, setImgData] = useState([])
+  const reqUrl = "https://api.giphy.com/v1/gifs/translate?api_key=l9wddb2iB91ZMFFD1U2BXvF4C5wubzBG&s=funny"
 
+  useEffect(() => {
+    async function getData() {
+      try {        
+        const res = await fetch(reqUrl)
+        
+        if (res.ok) {
+          let parsed = await res.json()
+          let imgUrl = parsed.data.images.original.url
+          if (imgData.length <= 20) {
+            setImgData(prevImg => [...prevImg, imgUrl])
+            console.log(imgUrl);
+            console.log(imgData);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getData()
+  },[imgData]) 
   return (
     <>
     <h1 className="text-3xl font-bold underline">
